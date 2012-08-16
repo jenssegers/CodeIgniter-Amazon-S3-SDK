98b79ff89dfd40190cf6e361f70587d8a0deaf65
Jonathon Hill <jhill@brandmovers.com>
2012-08-16 15:56:50 -0400

    Fixed a bug where the s3.php config file settings were not being loaded properly in s3::initialize()

--- config/s3.php
+++ config/s3.php
@@ -1,10 +1,28 @@
 <?php
+
+/*
+CodeIgniter Amazon S3 library
+=============================
+
+This is an adjusted version of the Amazon S3 SDK by tpyo. It allows
+loading the library in a CodeIgniter like manner. Edit the s3.php
+config file and load the library:
+
+	$this->load->library('s3');
+	
+The interface of the class has not been adjusted, the original
+documentation is located at:
+
+https://github.com/tpyo/amazon-s3-php-class/blob/master/README.txt
+
+*/
+
 /*
 | -------------------------------------------------------------------
 | Amazon S3 Configuration
 | -------------------------------------------------------------------
 */
 
-$config["accessKey"] = "";
-$config["secretKey"] = "";
-$config["useSSL"] = FALSE;
+$config['accessKey'] = '';
+$config['secretKey'] = '';
+$config['useSSL'] = FALSE;
